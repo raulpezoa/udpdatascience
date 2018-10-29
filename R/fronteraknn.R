e9c94e1bfdd571c65b6ebcb88ffdc518a1fc08dd
raulpezoa <raul.pezoa@gmail.com>
2018-10-29 11:51:19 -0300

    Agregadas funciones knn

---
+++ R/fronteraknn.R
@@ -0,0 +1,45 @@
+#' Grafica frontera de decision para knn en dos variables
+#' @param modelo Objeto knn
+#' @param data Dataframe correspondiente a los puntos a graficar
+#' @param prob Probabilidad de corte para decidir clase
+#' @return Grafico con frontera de decision
+#' @export 
+fronteraknn <- function(modelo,data,prob){
+  options(warn=-1)
+  suppressMessages(library(ROCR))
+  suppressMessages(library(data.table))
+  suppressMessages(library(scales))
+  options(warn=0)
+  
+  data <- data.table(data)
+  if (length(modelo$coefficients)!=3) {
+    stop("Funciona para modelo con dos predictores")
+  }
+  
+  y=row.names(attr(modelo$terms,"factors"))[1]
+  x1=row.names(attr(modelo$terms,"factors"))[2]
+  x2=row.names(attr(modelo$terms,"factors"))[3]
+
+  data$y <- unlist(data[,y,with=F])
+  data$x1num <- unlist(data[,x1,with=F])
+  data$x2num <- unlist(data[,x2,with=F])
+  
+  m <- 101
+  grid <- expand.grid(x1num=seq(min(data$x1num),max(data$x1num),length=m),
+                      x2num=seq(min(data$x2num),max(data$x2num),length=m))
+  grid2 <- grid
+  names(grid2) <- c(x1,x2)
+  yhat <- predict(modelo,grid2,type="prob")[,2]
+  yhat <- ifelse(yhat>prob,"red","blue")
+  
+  ###################
+  xmin<-min(data$x1num,na.rm = T)
+  xmax<-max(data$x1num,na.rm = T)
+  ymin<-min(data$x2num,na.rm = T)
+  ymax<-max(data$x2num,na.rm = T)
+  plot(grid, col = alpha(yhat,0.1), pch = 20, cex = 1,xlim=c(xmin,xmax),ylim=c(ymin,ymax),xlab="",ylab="",xaxt="n",yaxt="n")
+  par(new=T)
+  plot(data[data$y==0,]$x1num,data[data$y==0,]$x2num,col="blue",pch=20,cex=0.3,main="",xlim=c(xmin,xmax),ylim=c(ymin,ymax),cex.lab=1.5, cex.axis=1.5, cex.main=1.5,xlab=x1,ylab=x2)
+  par(new=T)
+  plot(data[data$y==1,]$x1num,data[data$y==1,]$x2num,col="red",pch=20,cex=1,xlim=c(xmin,xmax),ylim=c(ymin,ymax),xaxt="n",yaxt="n",cex.lab=1.5, cex.axis=1.5, cex.main=1.5,xlab="",ylab="")
+}
--- R/probcorte.R
+++ R/probcorte.R
@@ -4,26 +4,43 @@
 #' @param spec Especificidad buscada
 #' @return Curva ROC, AUC y probabilidad de corte
 #' @export 
-probcorte <- function(modelo,data,spec){
+probcorte <- function(modelo,data,spec,tipo="glm"){
   options(warn=-1)
   suppressMessages(library(ROCR))
   suppressMessages(library(data.table))
   suppressMessages(library(scales))
   options(warn=0)
   data <- data.table(data)
-  y=all.vars(formula(modelo))[1]
-  data$y <- unlist(data[,y,with=F])
-  predprob <- predict(modelo,data,type='response')
-  pr <- prediction(predprob, data$y)
-  perf <- performance(pr, measure = "sens", x.measure = "spec")
-  plot(perf)
-  
-  cutoffs <- data.frame(cut=perf@alpha.values[[1]], spec=perf@x.values[[1]], 
-                        sens=perf@y.values[[1]])
-  prob <- cutoffs[cutoffs$spec<=spec,]
-  prob <- prob$cut[1]
-  
-  perf <- performance(pr, measure = "auc")
-  auc <- unlist(perf@y.values)
-  return(list(prob=prob,auc=auc))
+  if (tipo=="glm"){
+    y=all.vars(formula(modelo))[1]
+    data$y <- unlist(data[,y,with=F])
+    predprob <- predict(modelo,data,type='response')
+    pr <- prediction(predprob, data$y)
+    perf <- performance(pr, measure = "sens", x.measure = "spec")
+    plot(perf)
+    
+    cutoffs <- data.frame(cut=perf@alpha.values[[1]], spec=perf@x.values[[1]], 
+                          sens=perf@y.values[[1]])
+    prob <- cutoffs[cutoffs$spec<=spec,]
+    prob <- prob$cut[1]
+    
+    perf <- performance(pr, measure = "auc")
+    auc <- unlist(perf@y.values)
+    return(list(prob=prob,auc=auc))
+  } else if (tipo=="knn"){
+    y=row.names(attr(modelo$terms,"factors"))[1]
+    data$y <- unlist(data[,y,with=F])
+    predprob <- predict(modelo,data,type='prob')[,2]
+    pr <- prediction(predprob, data$y)
+    perf <- performance(pr, measure = "sens", x.measure = "spec")
+    plot(perf)
+    
+    cutoffs <- data.frame(cut=perf@alpha.values[[1]], spec=perf@x.values[[1]], 
+                          sens=perf@y.values[[1]])
+    prob <- cutoffs[cutoffs$spec>=spec,]
+    prob <- prob$cut[nrow(prob)]
+    perf <- performance(pr, measure = "auc")
+    auc <- unlist(perf@y.values)
+    return(list(prob=prob,auc=auc))
+  }
 }
