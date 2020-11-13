482dfe367b3da77dcff270e6862e9f3fca38e63e
raulpezoa <39779169+raulpezoa@users.noreply.github.com>
2020-11-13 09:29:56 -0300

    Add files via upload

---
+++ R/prune2.R
@@ -0,0 +1,14 @@
+#' Poda arbol en profundidad deseada
+#' @param arbol arbol profundo
+#' @param i nivel deseado
+#' @export 
+
+prune2 <- function(arbol,i){
+  options(warn=-1)
+  suppressMessages(library(rpart))
+  options(warn=0)
+  cp <- arbol$cptable[,1]
+  cp.deseado <- cp[i]*1.0000001
+  arbol.podado <- prune(arbol,cp=cp.deseado)
+  return(arbol.podado)
+}
